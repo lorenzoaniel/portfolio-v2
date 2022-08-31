882c625ed5af9349168264232fa5ca44a89aef51
lorenzoaniel <lorenzoaniel@gmail.com>
2022-08-31 13:07:21 -0600

    format update
    lowered spaces, prettier

--- src/Styles/GlobalStyles/normalizeStyle.tsx
+++ src/Styles/GlobalStyles/normalizeStyle.tsx
@@ -1,26 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
 const NormalizeStyle = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        padding: 0;
-        margin: 0;
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    height: 100vh;
+    width: 100vw;
+
+    body {
+      height: 100%;
+      width: 100%;
+
+      #root {
+        height: inherit;
+        width: inherit;
+      }
     }
-
-    html {
-        height: 100vh;
-        width: 100vw;
-
-        body {
-            height: 100%;
-            width: 100%;
-
-            #root {
-                height: inherit;
-                width: inherit; 
-            }
-        }
-    }
+  }
 `;
 
 export default NormalizeStyle;
