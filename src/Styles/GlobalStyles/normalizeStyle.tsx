6594ec4848b78d8399daf981a360af57270fad7d
lorenzoaniel <lorenzoaniel@gmail.com>
2022-08-30 18:52:20 -0600

    Added normalize values
    makes sure all containers take full viewport availability, no padding or margins

--- src/Styles/GlobalStyles/normalizeStyle.tsx
+++ src/Styles/GlobalStyles/normalizeStyle.tsx
@@ -1,7 +1,26 @@
 import { createGlobalStyle } from "styled-components";
 
-const normalizeStyle = createGlobalStyle`
-    
+const NormalizeStyle = createGlobalStyle`
+    * {
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    html {
+        height: 100vh;
+        width: 100vw;
+
+        body {
+            height: 100%;
+            width: 100%;
+
+            #root {
+                height: inherit;
+                width: inherit; 
+            }
+        }
+    }
 `;
 
-export default normalizeStyle;
+export default NormalizeStyle;
