7e5606f03378192986f892605bb570f4592f9a7d
lorenzoaniel <lorenzoaniel@gmail.com>
2022-08-31 16:18:04 -0600

    useContext for API data added
    added useContext API so i dont prop drill all the way to Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState, createContext } from "react";
 import routesPageData from "./Api/RoutesPageData";
 import Layout from "./Components/Layout/Layout";
 import About from "./Routes/About/About";
@@ -8,29 +8,31 @@ import NotFoundPage from "./Routes/NotFoundPage/NotFoundPage";
 import Projects from "./Routes/Projects/Projects";
 import NormalizeStyle from "./Styles/GlobalStyles/normalizeStyle";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <NormalizeStyle />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<About title={routesPageData.about.title} />} />
-          <Route
-            path="projects"
-            element={<Projects title={routesPageData.projects.title} />}
-          />
-          <Route
-            path="contact"
-            element={<Contact title={routesPageData.contact.title} />}
-          />
-          <Route
-            path="*"
-            element={<NotFoundPage title={routesPageData.notFoundPage.title} />}
-          />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+export const NavbarContext = createContext(routesPageData);
 
-export default App;
+export default function App() {
+	const [routePageData, setRoutePageData] = useState(routesPageData);
+
+	return (
+		<BrowserRouter>
+			<NormalizeStyle />
+			<Routes>
+				<Route path={routePageData.about.path} element={<Layout />}>
+					<Route index element={<About title={routePageData.about.title} />} />
+					<Route
+						path={routePageData.projects.path}
+						element={<Projects title={routePageData.projects.title} />}
+					/>
+					<Route
+						path={routePageData.contact.path}
+						element={<Contact title={routePageData.contact.title} />}
+					/>
+					<Route
+						path={routePageData.notFoundPage.path}
+						element={<NotFoundPage title={routePageData.notFoundPage.title} />}
+					/>
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
+}
