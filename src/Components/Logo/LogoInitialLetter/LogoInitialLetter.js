5c1ee969492f7dcc6e2132f7629679e3e738941b
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-02 21:04:45 -0600

    Create LogoInitialLetter.js
    Created base logo 'MLA' letters, container with span and individual letters per span

---
+++ src/Components/Logo/LogoInitialLetter/LogoInitialLetter.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const LogoInitialLetterDiv = styled(motion.div)`
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+`;
+
+const LogoInitialLetterSpan = styled(motion.span)`
+	font-family: "Special Elite", cursive;
+	font-size: 5vw;
+`;
+
+export default function LogoInitialLetter(props) {
+	const { children, initialLetter } = props;
+
+	return (
+		<LogoInitialLetterDiv>
+			<LogoInitialLetterSpan>{initialLetter}</LogoInitialLetterSpan>
+			{children}
+		</LogoInitialLetterDiv>
+	);
+}
