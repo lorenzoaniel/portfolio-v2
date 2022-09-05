48662bdd4020ab142c94beead3514a2e3beceea9
lorenzoaniel <lorenzoaniel@gmail.com>
2022-09-04 20:24:18 -0600

    Update OutletSectionContainer.js
    cleanup

--- src/Components/OutletSectionContainer/OutletSectionContainer.js
+++ src/Components/OutletSectionContainer/OutletSectionContainer.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const OutletSectionContainerDiv = styled(motion.div)`
-	background-color: peachpuff; //for development
+	height: 100%;
+	grid-area: outlet;
 `;
 
 export default function OutletSectionContainer(props) {
